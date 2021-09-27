c990e42c6be70e2403e4e3203e8596a71b850d52
matias <matiasmicheletto@gmail.com>
2021-09-26 22:22:48 -0300

    Estilos vista medicion velocidad

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -5,10 +5,10 @@ const CustomInput = props => {
     return (
         <div className={classes.Container} style={props.style}>
             <ListInput
-                {...props}
                 outline
                 floatingLabel
                 clearButton
+                {...props}
                 ></ListInput>
             {props.unit ? <span className={classes.UnitLabel}>{props.unit}</span> : null}
         </div>
--- src/components/Velocity/index.js
+++ src/components/Velocity/index.js
@@ -1,4 +1,4 @@
-import { Page, Navbar, Block, List, Row, Col } from "framework7-react";
+import { Page, Navbar, Block, List, Row, Col, Button } from "framework7-react";
 import CustomInput from "../Inputs";
 import DistanceIcon from "../../img/icons/distancia.png"
 import classes from './Velocity.module.css';
@@ -6,10 +6,83 @@ import moment from 'moment';
 import Timer from './Timer';
 import Toast from "../Toast";
 import { useState } from "react";
+import { FaPlay, FaStop, FaPlus, FaMinus } from 'react-icons/fa';
 
 const timer = new Timer;
 
-const Velocity = props => {
+const InputBlock = props => ( // Input de distancia
+    <List form noHairlinesMd className={classes.Form}>
+        <Row slot="list">
+            <Col width={20}></Col>
+            <Col width={60}>
+                <CustomInput
+                    value={props.distance || ''}
+                    label="Dist. recorrida"
+                    type="number"
+                    unit="m"
+                    onChange={props.onChange}
+                ></CustomInput>
+            </Col>
+            <Col width={20}></Col>
+        </Row>
+    </List>
+);
+
+const PlayButton = props => ( // Boton de control del cronometro
+    <Button style={{minHeight:50}} onClick={props.onClick}>
+        {
+            props.running ? 
+                <FaStop color="red" size={40}/>
+            :
+                <FaPlay color="green" size={40}/>
+        }
+    </Button>
+);
+
+const DataTable = props => ( // Tabla de resultados parciales
+    <table className={`data-table ${classes.Table}`}>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Tiempo</th>
+                <th>velocidad</th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                props.data.map((d, idx) => (
+                    <tr key={idx}>
+                        <td>{idx+1}</td>
+                        <td>{d.time/1000} seg.</td>
+                        <td>{d.vel.toFixed(2)} Km/h</td>
+                    </tr>
+                ))
+            }
+        </tbody>
+    </table>
+);
+
+const OutputBlock = props => ( // Bloque con resultado final a exportar
+    <List form noHairlinesMd style={{marginTop:"0px"}}>
+        <Row slot="list">
+            <Col width={10}></Col>
+            <Col width={80}>
+                <CustomInput
+                    readOnly
+                    value={props.output}
+                    label="Vel. promedio"
+                    type="number"
+                    unit="Km/h"
+                    clearButton={false}
+                ></CustomInput>
+            </Col>
+            <Col width={10}></Col>
+        </Row>
+    </List>
+);
+
+
+const Velocity = () => { // View
     const [time, setTime] = useState(0);     
     
     const [running, setRunning] = useState(0);
@@ -17,6 +90,8 @@ const Velocity = props => {
     const [distance, setDistance] = useState(50);
 
     const [data, setData] = useState([]);
+
+    const [pushEnabled, setPushEnabled] = useState(false);
     
     const toggleRunning = () => {
         if(!running){
@@ -28,6 +103,7 @@ const Velocity = props => {
             timer.callback = ()=>{};            
         }
         setRunning(!running);
+        setPushEnabled(running);
     };
 
     const getTime = () => {
@@ -44,69 +120,70 @@ const Velocity = props => {
             })));
         }else
             Toast("error", "Ingrese un valor de distancia mayor que 0 m", 2000, "center");
-    }
-
-    const pushData = () => {
-        const temp = [...data];
-        temp.push({
-            time: time,
-            vel: distance/time*3600
-        });        
-        setData(temp);
     };
+
+    const pushData = () => { // Agregar dato medido
+        if(data.length < 3 && time > 0){
+            const temp = [...data];
+            temp.push({
+                time: time,
+                vel: distance/time*3600
+            });        
+            setData(temp);
+            setPushEnabled(false);
+        }
+    };
+
+    const popData = () => { // Quitar último dato medido
+        if(data.length > 0){
+            const temp = [...data];
+            temp.pop();
+            setData(temp);
+            setPushEnabled(true);
+        }
+    };
+
+    const dataAvg = () => data.length > 0 ? data.reduce((r, a) => a.vel + r, 0)/data.length : 0;
 
     return (
         <Page>
             <Navbar title="Cronómetro" style={{maxHeight:"40px", marginBottom:"0px"}}/>      
             <Block>
                 <img src={DistanceIcon} className={classes.Icon} alt="distance" />
-                <List form noHairlinesMd className={classes.Form}>
-                    <Row slot="list">
-                        <Col width={20}></Col>
-                        <Col width={60}>
-                            <CustomInput
-                                value={distance || ''}
-                                label="Dist. recorrida"
-                                type="number"
-                                unit="m"
-                                onChange={v=>updateDistance(parseInt(v.target.value))}
-                            ></CustomInput>
-                        </Col>
-                        <Col width={20}></Col>
-                    </Row>
-                </List>
+                <InputBlock onChange={v=>updateDistance(parseInt(v.target.value))} distance={distance}/>
             </Block>
             <Block style={{marginTop:"60px", textAlign:"center"}}>
-                <div>
-                    <p style={{fontSize:"50px"}}>{getTime()}</p>
-                    <input type='button' onClick={toggleRunning} value={running ? "Detener":"Iniciar"}/>
-                </div>
+                <p style={{fontSize:"50px", margin:"0px"}}>{getTime()}</p>
+                <PlayButton onClick={toggleRunning} running={running} />
+            </Block>
+            <Block style={{marginBottom: "0px",textAlign:"center"}}>
+                <Row style={{alignItems:"center"}}>
+                    <Col width={20}>
+                        <Row>
+                            <Button disabled={!pushEnabled} onClick={pushData}>
+                                <FaPlus  color="green" size={30}/>
+                            </Button>
+                        </Row>
+                        <Row>
+                            <Button disabled={running || data.length===0} onClick={popData}>
+                                <FaMinus color="red" size={30}/>
+                            </Button>
+                        </Row>
+                    </Col>
+                    <Col width={80}>
+                        <DataTable data={data} />
+                    </Col>
+                </Row>
+            </Block>
+            <Block style={{marginTop:"0px",textAlign:"center"}}>
+                <OutputBlock output={dataAvg().toFixed(2)}/>
             </Block>
             <Block style={{textAlign:"center"}}>
-                <input type='button' onClick={pushData} value="Agregar"/>
-                <table className="data-table">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Tiempo</th>
-                            <th>velocidad</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            data.map((d, idx) => (
-                                <tr key={idx}>
-                                    <td>{idx+1}</td>
-                                    <td>{d.time/1000} seg.</td>
-                                    <td>{d.vel.toFixed(2)} Km/h</td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
-            </Block>
-            <Block style={{textAlign:"center"}}>
-                <p>Exportar</p>    
+                <Row>
+                    <Col width={20}></Col>
+                    <Col width={60}><Button fill>Exportar</Button></Col>
+                    <Col width={20}></Col>
+                </Row>
             </Block>
         </Page>
     );
